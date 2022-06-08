e4c45b8981b19291fea3f079e3547a48eeaa2be8
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-08 21:14:32 +0900

    페이징

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -36,4 +36,14 @@ public class Member {
     public void setAge(int age) {
         this.age = age;
     }
+
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
